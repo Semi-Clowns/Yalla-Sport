35f026d1c448742fbfcbe14875a8087cde63f553
mahmoud raafat <mahmoudrafatt995@gmail.com>
2026-05-04 23:29:54 -0700

    Initial commit with proper .gitignore

---
+++ SportsApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApplication
+//
+//  Created by Mahmoud  Raafat  on 04/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
