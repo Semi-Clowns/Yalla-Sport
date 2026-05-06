12059bab2a1774a94eadf96d035efcd11691efcd
mahmoud raafat <mahmoudrafatt995@gmail.com>
2026-05-06 16:56:39 -0700

    added colors swift file to add application colors in the class ui color , added onboarding image and added the presenter of the on boarding view

---
+++ SportsApplication/Modules/Onboadrding/Presenter/OnboardingPresenter.swift
@@ -0,0 +1,63 @@
+//
+//  OnboardingPresenter.swift
+//  SportsApplication
+//
+//  Created by Mahmoud  Raafat  on 06/05/2026.
+//
+
+import Foundation
+import Foundation
+
+protocol OnboardingPresenterProtocol: AnyObject {
+    var pageCount: Int { get }
+    func viewDidLoad()
+    func nextTapped(currentIndex: Int)
+    func didSwipeToPage(index: Int)
+    func skipTapped()
+    func getPages() -> [String]
+}
+
+class OnboardingPresenter: OnboardingPresenterProtocol {
+
+    weak var view: OnboardingViewProtocol?
+    private let pageIdentifiers: [String] = ["page1", "page2"]
+    private let localDataManager: LocalDataManagerProtocol
+    var pageCount: Int {
+        pageIdentifiers.count
+    }
+
+    init(view: OnboardingViewProtocol,localDataManager: LocalDataManagerProtocol = LocalDataManager.shared) {
+        self.view = view
+        self.localDataManager = localDataManager
+        
+    }
+
+    func getPages() -> [String] {
+        return pageIdentifiers
+    }
+
+    func viewDidLoad() {
+        view?.updateUI(index: 0, isLastPage: pageCount == 1)
+    }
+
+    func nextTapped(currentIndex: Int) {
+        guard currentIndex < pageCount else { return }
+        let nextIndex = currentIndex + 1
+        if nextIndex < pageCount {
+            view?.updateUI(index: nextIndex, isLastPage: nextIndex == pageCount - 1)
+        } else {
+            localDataManager.setHasSeenOnboarding(true)
+            view?.navigateToMainApp()
+        }
+    }
+
+    func didSwipeToPage(index: Int) {
+        guard index >= 0, index < pageCount else { return }
+        view?.updateUI(index: index, isLastPage: index == pageCount - 1)
+    }
+
+    func skipTapped() {
+        localDataManager.setHasSeenOnboarding(true)
+        view?.navigateToMainApp()
+    }
+}
---
+++ SportsApplication/Utils/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  SportsApplication
+//
+//  Created by Mahmoud  Raafat  on 06/05/2026.
+//
+
+import Foundation
+import UIKit
+extension UIColor {
+    
+    static let primaryText = UIColor(named: "PrimaryTextColor")
+    static let primaryColor = UIColor(named: "PrimaryColor")
+    static let BackgroundColor = UIColor(named: "BackgroundColor")
+
+    static let secondaryText = UIColor(named: "secondaryextColor")
+    
+}
+
