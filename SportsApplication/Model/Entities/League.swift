6938e993833dd152d1c38794d88b5e410a96ff2c
Moaz Osama <moazosama204@gmail.com>
2026-05-07 12:20:50 -0700

    add league entity

---
+++ SportsApplication/Model/Entities/League.swift
@@ -0,0 +1,16 @@
+//
+//  League.swift
+//  SportsApplication
+//
+//  Created by Moaz on 07/05/2026.
+//
+
+import Foundation
+
+struct League : Codable {
+    let id : Int
+    let leagueName: String?
+    let leagueLogo: String?
+    let countryName: String?
+    let countryLogo: String?
+}
