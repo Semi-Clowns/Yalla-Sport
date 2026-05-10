546119395f6eb7be69924cf551b0a9b21a934a80
Moaz Osama <moazosama204@gmail.com>
2026-05-10 09:26:21 -0700

    solve merge conflicts

--- SportsApplication/Modules/Leagues/View/Controller/AllLeaguesViewController.swift
+++ SportsApplication/Modules/Leagues/View/Controller/AllLeaguesViewController.swift
@@ -49,7 +49,6 @@ class AllLeaguesViewController: UIViewController , AllLeaguesViewControllerProto
         leaguesTableView.register(UINib(nibName: "LeagueTableViewCell", bundle: nil), forCellReuseIdentifier: "LeagueTableViewCell")
         
         
-        
         // search bar
 //        leaguesSearchBar.layoutMargins = UIEdgeInsets(  top: 0, left: 0, bottom: 0, right: 0)
     }
@@ -64,6 +63,7 @@ class AllLeaguesViewController: UIViewController , AllLeaguesViewControllerProto
         // Pass the selected object to the new view controller.
     }
     */
+
 
 }
 
