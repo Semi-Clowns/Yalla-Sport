31729c01e2c82f9fd8d6f3279049fca5fd33ea97
Muaz <102158567+Moazosama2004@users.noreply.github.com>
2026-05-13 13:36:18 +0300

    Refactor NetworkProtocol by removing sport-specific methods
    Removed specific fixture and team retrieval methods for various sports.

--- SportsApplication/Model/Services/Networking/NetworkProtocol.swift
+++ SportsApplication/Model/Services/Networking/NetworkProtocol.swift
@@ -15,38 +15,4 @@ protocol NetworkProtocol {
         responseType: T.Type,
         completion: @escaping (Result<T, Error>) -> Void
     )
-    
-    func getFootballFixtures(
-        leagueId: Int,
-        from: String,
-        to: String,
-        completion: @escaping (Result<[Event], Error>) -> Void
-    )
-    
-    func getBasketballFixtures(
-        leagueId: Int,
-        from: String,
-        to: String,
-        completion: @escaping (Result<[Event], Error>) -> Void
-    )
-    
-    func getTennisFixtures(
-        leagueId: Int,
-        from: String,
-        to: String,
-        completion: @escaping (Result<[Event], Error>) -> Void
-    )
-    
-    func getCricketFixtures(
-        leagueId: Int,
-        from: String,
-        to: String,
-        completion: @escaping (Result<[Event], Error>) -> Void
-    )
-    
-    func getTeamsFrom(
-        leagueId: Int,
-        sport: String,
-        completion: @escaping (Result<[Team], Error>) -> Void
-    )
 }
