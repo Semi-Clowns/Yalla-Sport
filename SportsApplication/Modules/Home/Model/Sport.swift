cc82e13ce182618efe275890f7b56580029c3d2c
Moaz Osama <moazosama204@gmail.com>
2026-05-13 13:56:32 -0700

    make providing sports from singleton class on home screen

--- SportsApplication/Modules/Home/Model/Sport.swift
+++ SportsApplication/Modules/Home/Model/Sport.swift
@@ -3,3 +3,29 @@ struct Sport {
     let image : String
     let type : String
 }
+
+class SportManager {
+    
+    static let shared = SportManager()
+    
+    private let sports: [Sport] = [
+        Sport(title: "Football", image: "football_bg" ,type: "football"),
+        Sport(title: "BasketBall", image: "basketball_bg", type: "basketball"),
+        Sport(title: "Cricket", image: "cricket_bg", type: "cricket"),
+        Sport(title: "Tennis", image: "tennis_bg", type: "tennis")
+    ]
+    
+    private init(){}
+    
+    func getSports() -> [Sport] {
+        self.sports
+    }
+    
+    func getSportAtIndex(at index: Int) -> Sport {
+        self.sports[index]
+    }
+    
+    func getSportsCount() -> Int {
+        self.sports.count
+    }
+}
--- SportsApplication/Modules/Home/Presenter/HomePresenter.swift
+++ SportsApplication/Modules/Home/Presenter/HomePresenter.swift
@@ -1,6 +1,6 @@
 final class HomePresenter : HomePresenterProtocol {
     private let networkMonitor: NetworkMonitor
-    var sports = [Sport]()
+    private let sportsManager = SportManager.shared
     weak var view : HomeViewControllerProtocol?
     
     func attachView(with view: HomeViewControllerProtocol) {
@@ -8,25 +8,19 @@ final class HomePresenter : HomePresenterProtocol {
     }
     
     init(networkMonitor: NetworkMonitor = NetworkMonitor.shared) {
-        sports.append(Sport(title: "Football", image: "football_bg" ,type: "football"))
-        sports.append(Sport(title: "BasketBall", image: "basketball_bg", type: "basketball"))
-        sports.append(Sport(title: "Cricket", image: "cricket_bg", type: "cricket"))
-        sports.append(Sport(title: "Tennis", image: "tennis_bg", type: "tennis"))
         self.networkMonitor = networkMonitor
     }
-    
-    
-    
+
     func getSportsCount() -> Int {
-         return sports.count
+        return sportsManager.getSportsCount()
     }
     
     func getSportItem(index : Int) -> Sport {
-         return sports[index]
+        return sportsManager.getSportAtIndex(at: index)
     }
     
     func getSelectedSport(at index: Int) -> String {
-        sports[index].type
+        sportsManager.getSportAtIndex(at: index).type
     }
     func navigateToDisplayLeague() {
         if networkMonitor.isConnected{
