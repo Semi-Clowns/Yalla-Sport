bce625e7795f153b2cf6d524bd648e313199f68f
Moaz Osama <moazosama204@gmail.com>
2026-05-13 17:16:44 -0700

    complete ui screen

---
+++ SportsApplication/View/Cell/PlayerInfoTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  PlayerInfoTableViewCell.swift
+//  SportsApplication
+//
+//  Created by Moaz on 13/05/2026.
+//
+
+import UIKit
+import Kingfisher
+
+class PlayerInfoTableViewCell: UITableViewCell {
+
+    @IBOutlet private var infoValueLabel: UILabel!
+    @IBOutlet private var infoImageView: UIImageView!
+    @IBOutlet private var infoTitleLabel: UILabel!
+    
+    @IBOutlet var containerView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        infoImageView.layer.cornerRadius = infoImageView.frame.width / 2
+        
+        containerView.layer.cornerRadius = 32
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func config(
+        infoTitle: String,
+        infoImage: String?,
+        infoValue: String?
+    ) {
+        infoTitleLabel.text = infoTitle
+        if infoImage != nil {
+            infoImageView.kf.setImage(with: URL(string: infoImage ?? "" ),
+                                      placeholder: UIImage(systemName: "photo"))
+        } else {
+            infoImageView.isHidden = true
+        }
+       
+        infoValueLabel.text = infoValue
+    }
+    
+}
