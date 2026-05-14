bac966527d5bc43f36e213fa7f45dc4dfa8182e1
Moaz Osama <moazosama204@gmail.com>
2026-05-13 23:31:18 -0700

    add testing with Expectations only

--- SportsApplicationTests/SportsApplicationTests.swift
+++ SportsApplicationTests/SportsApplicationTests.swift
@@ -6,31 +6,33 @@
 //
 
 import XCTest
+import Alamofire
 @testable import SportsApplication
 
 class SportsApplicationTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var networkService: NetworkService!
+    
+    override func setUp() {
+        super.setUp()
+        networkService = NetworkService()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_fetchAllLeagues_returnsData() throws {
+        let expectation = expectation(description: "API returns Leagues")
+        
+        networkService.getAllLeagues(sport: "football", responseType: LeagueResponse.self) { result in
+            switch result {
+                case .success(let response):
+                    XCTAssertNotNil(response)
+                print("Leagues count: \(response.result.count )")
+                case .failure(let error):
+                    XCTFail("Request failed: \(error.localizedDescription)")
+                }
+                expectation.fulfill()
         }
+        
+        waitForExpectations(timeout: 15)
     }
-
+    
 }
