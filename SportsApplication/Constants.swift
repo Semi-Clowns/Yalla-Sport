0edc3d4036d8821ed5a48682a3c4fbd4e9903bf1
mahmoud raafat <mahmoudrafatt995@gmail.com>
2026-05-13 17:51:53 -0700

    Stop tracking .xcconfig

--- SportsApplication/Constants.swift
+++ SportsApplication/Constants.swift
@@ -9,5 +9,11 @@ import Foundation
 
 struct Constants {
     static let baseURL = "https://apiv2.allsportsapi.com"
-    static let apiKey = "1bff5ab11b76f21da638a27364d5b5852e7d452e46efce876cea77943d907e5c" // we will enhance this weird way later.
+    static var apiKey: String {
+            guard let key = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
+                fatalError("API_KEY not found")
+            }
+            return key
+    }
+    
 }
