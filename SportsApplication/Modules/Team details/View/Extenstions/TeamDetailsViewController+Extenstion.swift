39716982e7ab192d1bf35c765255cd9cbf75cba8
mahmoud raafat <mahmoudrafatt995@gmail.com>
2026-05-14 03:46:06 -0700

    redesign onboarding screens , deleted import unlit in coredata , added epmtystate for teams screen and deleted all league header

--- SportsApplication/Modules/Team details/View/Extenstions/TeamDetailsViewController+Extenstion.swift
+++ SportsApplication/Modules/Team details/View/Extenstions/TeamDetailsViewController+Extenstion.swift
@@ -10,27 +10,52 @@ import UIKit
 extension TeamDetailsViewController: UITableViewDelegate, UITableViewDataSource {
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+           if presenter?.isFullyEmpty() ?? true { return 0 }
+           return 2
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return section == 0 ? presenter?.getTeamCoachCount() ?? 0 : presenter?.getTeamPlayersCount() ?? 0
+        guard let presenter = presenter else { return 0 }
+              if section == 0 {
+                  return presenter.hasCoach() ? presenter.getTeamCoachCount() : 1
+              } else {
+                  return presenter.hasPlayers() ? presenter.getTeamPlayersCount() : 1
+              }
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerTableViewCell", for: indexPath) as? PlayerTableViewCell else { return UITableViewCell() }
+           guard let presenter = presenter else { return UITableViewCell() }
 
-        guard let team = presenter?.getTeamDetails() else {return UITableViewCell()}
-        if indexPath.section == 0 {
-            guard let coach = presenter?.getCoach() else { return UITableViewCell() }
-            cell.configCell(for: coach.asPlayer())
-        } else {
-            guard let player = presenter?.getPlayerAtIndex(at: indexPath.row) else { return UITableViewCell() }
-            cell.configCell(for: player)
-        }
+           if indexPath.section == 0 && !presenter.hasCoach() {
+               return makeEmptyCell(in: tableView, for: indexPath, message: "No coach available")
+           }
 
-        return cell
-    }
+           if indexPath.section == 1 && !presenter.hasPlayers() {
+               return makeEmptyCell(in: tableView, for: indexPath, message: "No players available")
+           }
+
+           guard let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerTableViewCell", for: indexPath) as? PlayerTableViewCell else {
+               return UITableViewCell()
+           }
+
+           if indexPath.section == 0 {
+               guard let coach = presenter.getCoach() else { return UITableViewCell() }
+               cell.configCell(for: coach.asPlayer())
+           } else {
+               guard let player = presenter.getPlayerAtIndex(at: indexPath.row) else { return UITableViewCell() }
+               cell.configCell(for: player)
+           }
+
+           return cell
+       }
+
+       private func makeEmptyCell(in tableView: UITableView, for indexPath: IndexPath, message: String) -> UITableViewCell {
+           let cell = tableView.dequeueReusableCell(withIdentifier: "EmptyStateTeamsCell", for: indexPath)
+           if let emptyCell = cell as? EmptyStateTeamsCell {
+               emptyCell.messageLabel.text = message
+           }
+           return cell
+       }
 
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return section == 0 ? "Coach" : "Players"
@@ -38,7 +63,6 @@ extension TeamDetailsViewController: UITableViewDelegate, UITableViewDataSource 
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 110
-        return indexPath.section == 0 ? 90 : 80
     }
     
     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
